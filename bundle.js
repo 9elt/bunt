38e263cc4cea211b70a2fc1333d10cd0dc18de3d
cieffelle <lorenzo.cicuttin@recrowd.com>
2024-02-21 14:30:18 +0100

    generic filename

--- bundle.js
+++ bundle.js
@@ -1,42 +1,43 @@
 const [path, ...options] = Bun.argv.slice(2);
 
-const filename = path.split('/').pop().replace(/\.(js|ts)$/, "");
+const filename = path.split('/').pop();
+const id = filename.replace(/\.(js|ts)$/, "");
 
 const build = await Bun.build({
     entrypoints: [
         path.includes("src/") ? path :
-            "src/" + filename + ".js"
+            "src/" + filename
     ],
     outdir: './dist',
     minify: options.includes("--minify"),
 });
 
 if (!build.success) {
-    console.error(filename, "build failed.\n", error);
+    console.error(id, "build failed.\n", error);
     process.exit(1);
 }
 
 console.log(
     "\njs successfully built at:",
-    "\n  ./dist/" + filename + ".js"
+    "\n  ./dist/" + id + ".js"
 );
 
 if (!options.includes("--html"))
     process.exit(0);
 
-await Bun.write(`./dist/${filename}.html`, `\
+await Bun.write(`./dist/${id}.html`, `\
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>${filename.replace(/(\.|-|_)/g, " ")}</title>
+  <title>${id.replace(/(\.|-|_)/g, " ")}</title>
 </head>
-<body><script src="${filename}.js"></script></body>
+<body><script src="${id}.js"></script></body>
 </html>
 `);
 
 console.log(
     "\nhtml successfully built at:",
-    "\n  ./dist/" + filename + ".html"
+    "\n  ./dist/" + id + ".html"
 );
